28f701623c825d62f202906098633a47636b9274
Ahamed Rashid <167019273+rashidrzn8@users.noreply.github.com>
2025-02-27 11:17:57 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -7,11 +7,6 @@ Script Purpose:
     If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
 	
-WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
-*/
 
 USE master;
 GO
